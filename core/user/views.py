c22f1cdd8a22834439d9380ecc445124f3a3af80
ilyasosman <earlyosman@gmail.com>
2025-04-27 13:50:24 +0300

    chore: fix lint error

--- core/user/views.py
+++ core/user/views.py
@@ -1,5 +1,4 @@
-from rest_framework import generics, status, permissions
-from rest_framework.response import Response
+from rest_framework import generics, permissions
 from django.contrib.auth import get_user_model
 from .serializers import RegisterSerializer, UserSerializer
 
