668c05af371fe6d6a20b5770fcf7192eaec85d22
ilyasosman <earlyosman@gmail.com>
2025-04-28 16:08:32 +0300

    fix: lint warning

--- config/deployment_settings.py
+++ config/deployment_settings.py
@@ -1,7 +1,7 @@
 import os
 import dj_database_url
-from .settings import *
-from .settings import BASE_DIR
+from .settings import *  # noqa F403 F401
+from .settings import BASE_DIR  # noqa F401
 
 ALLOWED_HOSTS = [os.environ.get("RENDER_EXTERNAL_HOSTNAME")]
 CSRF_TRUSTED_ORIGINS = ["https://" + os.environ.get("RENDER_EXTERNAL_HOSTNAME")]
